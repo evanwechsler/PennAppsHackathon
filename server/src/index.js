d2385da15de20adb3aca62fdb35b33ae2f59f3ad
Pat <pwblais@gmail.com>
2021-09-11 18:24:28 -0400

    getAllUsers endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { collection, getDoc, doc } from "firebase/firestore"
+import { collection, getDoc, doc, where, query } from "firebase/firestore"
 import { Web3Storage } from 'web3.storage';
 import { File } from 'web3.storage';
 
@@ -34,22 +34,6 @@ const addPerson = async (req, res, next) => {
     }
 }
 
-const addVaccination = async (req, res, next) => {
-    try {
-        const data = req.body;
-        await db.collection('reference').doc(data['name']).get().then(doc => {
-            if (!doc.exists) {
-                res.status(404).send('User not found');
-            } else {
-
-            }
-        });
-        res.send('Vaccination Added');
-    } catch (error) {
-        res.status(400).send(error.message);
-    }
-}
-
 const getPerson = async (req, res, next) => {
     console.log(req.query);
     const params = req.query;
@@ -65,21 +49,6 @@ const getPerson = async (req, res, next) => {
         res.status(400).send(error.message);
     }
 }
-
-// const getPerson = async (req, res, next) => {
-//     console.log(req.query);
-//     const params = req.query;
-//     try {
-//         const result = await db.collection('reference').where('username', '==', params['username']).get()
-//         const person = result.docs.map((p) => p.data());
-
-//         if (person.length) {
-//             res.status(200).send("Found someone! " + person[0]['username']);
-//         }
-//     } catch (error) {
-//         res.status(400).send(error.message);
-//     }
-// }
 
 const createUser = async (req, res, next) => {
     const body = req.body;
@@ -103,6 +72,36 @@ const createUser = async (req, res, next) => {
     }
 }
 
+const login = async (req, res, next) => {
+    const body = req.body;
+    console.log(body);
+    try {
+        const referenceRef = collection(db, "reference");
+        const q = query(referenceRef, where("username", "==", body['username']), where("password", "==", body['password']));
+
+        const querySnapshot = await getDocs(q);
+        const found = querySnapshot.map((doc) => doc.data());
+        if (found) {
+            res.status(200).send('Found a login user');
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
+const getAllUsers = async (req, res, next) => {
+    const params = req.query;
+    try {
+        const referenceRef = collection(db, "reference");
+        const q = query(referenceRef, where("username", "!=", 0));
+        const querySnapshot = await getDocs(q);
+        const found = querySnapshot.map((doc) => doc.data()['username']);
+        res.status(200).send(found.json());
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+}
+
 const createVaccineRecord = async (req, res, next) => {
     const body = req.body;
     console.log(body);
@@ -120,11 +119,17 @@ const createVaccineRecord = async (req, res, next) => {
     }
 }
 
-const router = express.Router();
-router.post('/addperson', addPerson);
-router.get('/getperson', getPerson);
+const adminRouter = express.Router();
+const userRouter = express.Router();
 
-app.use('/api', router);
+adminRouter.post('/addperson', addPerson);
+adminRouter.get('/getperson', getPerson);
+adminRouter.post('/createuser', createUser);
+adminRouter.post('/createvaccinerecord', createVaccineRecord);
+
+
+app.use('/admin', adminRouter);
+app.use('/user', userRouter);
 
 
 app.listen(8080, () => console.log('Server is starting on url http://localhost:' + '8080'));
