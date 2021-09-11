41f57c3e0274884fbbbb932b537f54c79b2c0edf
evan-wechsler <evan.wechsler@gmail.com>
2021-09-10 22:14:09 -0400

    Created form to add a vaccine recor

--- adminapp/src/App.jsx
+++ adminapp/src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import Container from "@material-ui/core/Container";
+import Form from "./components/Form";
+import "./style.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Container>
+        <Form />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
