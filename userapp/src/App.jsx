1137f7ef72c0bab6d72c819fb5dfaf39a08a0ceb
Jason Rosner <38009477+jrosner1@users.noreply.github.com>
2021-09-10 22:11:53 -0400

    Got the sign in page set up in user app and it is ready to consume the API.

--- userapp/src/App.jsx
+++ userapp/src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import * as ROUTES from './routes'
+
+import LandingPage from './components/LandingPage'
+import Results from './components/Results'
+import { BrowserRouter as Router,
+  Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"> </div>;
+  return (
+    <div style={{display: "grid", placeItems: "center", height: "100vh"}}>
+      <Router>
+        <Route exact path = {ROUTES.SIGN_UP} component={LandingPage} />
+        <Route path = {ROUTES.RESULTS} component= {Results} />
+      </Router>
+    </div>
+  )
+
 }
 
 export default App;
---
+++ userapp/src/components/Results.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Results = () => {
+    return (
+    <h1>RESULTS</h1>
+    )
+}
+export default Results;
