c0ea153028b17d18902d3c7929cfa84192cd67b9
evan-wechsler <evan.wechsler@gmail.com>
2021-09-11 17:38:33 -0400

    Fixed minore styling issue in records

--- adminapp/src/views/UserProfile/components/Record.jsx
+++ adminapp/src/views/UserProfile/components/Record.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Record({ record }) {
   return (
     <div>
-      {Object.entries(record).map((key, value) => (
+      {Object.entries(record).map(([key, value]) => (
         <p>
           {key}: {value}
         </p>
